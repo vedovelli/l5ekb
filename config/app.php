a14fb356785d42b031b093998a600cdde141fdcf
Fabio Vedovelli <vedovelli@gmail.com>
2016-07-30 13:08:39 -0300

    Configuracao LaravelCollective HTML FORM

--- config/app.php
+++ config/app.php
@@ -156,6 +156,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+        * Custom
+        */
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -202,6 +207,11 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        /**
+        * Custom
+        */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
