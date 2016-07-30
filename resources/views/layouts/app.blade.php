6cdc1e0eed3b2c750404e7b6201a7c08e37a8e7a
Fabio Vedovelli <vedovelli@gmail.com>
2016-07-30 12:14:17 -0300

    Criação do layout da app

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>L5EK-b @yield('page-title')</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
